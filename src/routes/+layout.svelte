f5a4be9d53b543f28fb699d8b971ba89182257ec
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2024-03-13 00:40:26 +0100

    better disclaimers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,13 @@ import '../app.postcss';
   <slot />
   <div slot="footer">
 	<p class="text-center text-gray-500 text-sm">
-		Made with ❤️ by OptoCloud
+		Made with ❤️ by <a class="text-blue-500 underline" href="https://x.com/optocloud">OptoCloud</a>
 	</p>
 	<p class="text-center text-gray-500 text-sm hidden sm:block">
 		Uptime of this service is not guaranteed. Data is webscraped from the FAA and may be delayed or inaccurate. This is a personal project and is not affiliated with SpaceX or the FAA.
 	</p>
 	<p class="text-center text-gray-500 text-sm hidden sm:block">
-		If this service is in conflict with any laws or regulations, please contact us at <a class="text-blue-500 underline" href="mailto:contact@optocloud.no">contact@optocloud.no</a> and we will take it down.
+		Please do not hesitate to reach out if you have any questions or concerns, you can contact me at <a class="text-blue-500 underline" href="mailto:contact@optocloud.no">contact@optocloud.no</a>
 	</p>
   </div>
 </AppShell>
