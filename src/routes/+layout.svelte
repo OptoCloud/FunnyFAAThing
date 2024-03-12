27b7f9bf1a1271bea12195114bb12387c9fa3d84
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2024-03-12 23:22:55 +0100

    Add disclaimer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import '../app.postcss';
+import '../app.postcss';
   import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
 <AppShell>
   <slot />
+  <div slot="footer">
+	<p class="text-center text-gray-500 text-sm">
+		Made with ❤️ by OptoCloud
+	</p>
+	<p class="text-center text-gray-500 text-sm hidden sm:block">
+		Uptime of this service is not guaranteed. Data is webscraped from the FAA and may be delayed or inaccurate. This is not an official SpaceX service. This is a fan project.
+	</p>
+  </div>
 </AppShell>
