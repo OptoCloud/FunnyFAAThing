add3131895c278870610658f0648dfbc41cdffd0
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2024-03-13 00:34:41 +0100

    More disclaimers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,10 @@ import '../app.postcss';
 		Made with ❤️ by OptoCloud
 	</p>
 	<p class="text-center text-gray-500 text-sm hidden sm:block">
-		Uptime of this service is not guaranteed. Data is webscraped from the FAA and may be delayed or inaccurate. This is not an official SpaceX service. This is a fan project.
+		Uptime of this service is not guaranteed. Data is webscraped from the FAA and may be delayed or inaccurate. This is a personal project and is not affiliated with SpaceX or the FAA.
+	</p>
+	<p class="text-center text-gray-500 text-sm hidden sm:block">
+		If this service is in conflict with any laws or regulations, please contact us at <a class="text-blue-500 underline" href="mailto:contact@optocloud.no">contact@optocloud.no</a> and we will take it down.
 	</p>
   </div>
 </AppShell>
