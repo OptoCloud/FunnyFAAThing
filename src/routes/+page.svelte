7698aec900fdcc3bcf120fc7d3673911e0cd065a
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2024-03-12 22:59:29 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,11 @@
     });
 </script>
 
-<div class="m-4 flex flex-col items-center gap-10">
+<svelte:head>
+    <title>SpaceX IFT-3 License Status</title>
+</svelte:head>
+
+<div class="container m-auto p-4 flex flex-col justify-center items-center gap-10">
     <h2 class="h2">Has SpaceX received the IFT-3 launch license modification yet?</h2>
     <h1 class={'h1 ' + (exists ? 'text-green-500' : 'text-red-500')}>{exists ? 'YES!!!\nIS POINTY END UP??' : 'Not Yet'}</h1>
     <div class="w-full h-[400px] md:h-[500px] overflow-y-auto table-container md:text-base">
