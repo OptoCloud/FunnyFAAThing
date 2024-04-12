94f85e2c14b5a83a43b9099058b97b51fd281bc9
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2024-04-12 14:00:09 +0200

    Update description and packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,21 +10,27 @@ import type { PageData } from './$types';
     let exists = data.faa_data.data.some((license) => {
         const isSpaceX = license.operatorName === 'SpaceX';
         const containsBocaChica = license.siteName.includes('Boca Chica');
-        const isMoreThanRev1 = license.licenseName.includes('Rev 2') || license.licenseName.includes('Rev 3');
+        const isMoreThanRev1 = license.licenseName.includes('Rev 3') || license.licenseName.includes('Rev 4');
         return isSpaceX && containsBocaChica && isMoreThanRev1;
     });
+
+    const title = 'SpaceX IFT-4 License Status';
+    const description = 'Has SpaceX received the IFT-4 launch license modification yet?';
+
+    const successMessage = 'YES!!!\nIS POINTY END UP??';
+    const failureMessage = 'Not Yet';
 </script>
 
 <svelte:head>
-    <title>SpaceX IFT-3 License Status</title>
+    <title>{title}</title>
 </svelte:head>
 
-<TwitterSummaryTags type="summary" title="SpaceX IFT-3 License Status" description="Has SpaceX received the IFT-3 launch license modification yet?" />
-<OpenGraphTags type="website" title="SpaceX IFT-3 License Status" description="Has SpaceX received the IFT-3 launch license modification yet?" url={$page.url.origin} />
+<TwitterSummaryTags type="summary" {title} {description} />
+<OpenGraphTags type="website" {title} {description} url={$page.url.origin} />
 
 <div class="container m-auto p-4 flex flex-col justify-center items-center gap-10">
-    <h2 class="h2">Has SpaceX received the IFT-3 launch license modification yet?</h2>
-    <h1 class={'h1 ' + (exists ? 'text-green-500' : 'text-red-500')}>{exists ? 'YES!!!\nIS POINTY END UP??' : 'Not Yet'}</h1>
+    <h2 class="h2">{description}</h2>
+    <h1 class={'h1 ' + (exists ? 'text-green-500' : 'text-red-500')}>{exists ? successMessage : failureMessage}</h1>
     <div class="w-full h-[400px] md:h-[500px] overflow-y-auto table-container md:text-base">
         <table class="table table-hover">
         <thead>
